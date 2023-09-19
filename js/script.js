21ba0cba27da7819f6f0524a7c30467bb3bfa157
Alethepata <alessia.patane@gmail.com>
2023-09-19 12:24:59 +0200

    [Add] randomize

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ const btn = document.getElementById('btn');
 
 container.classList.add('d-none');
 
+for( let r = 1; r <= 16; r++){
+let  num = randomize(1, 100);
+console.log(num); 
+}
+
 for( let i = 1; i <= 100; i++){
     const square = createSquare(i);
 
@@ -25,10 +30,20 @@ function createSquare(index){
     return squares;
 
 }
+
+function randomize(min, max){
+   return Math.floor(Math.random() * (max - min + 1) + min);
+   
+}
+
+
+
+
 btn.addEventListener('click', function(){
     container.classList.remove('d-none'); 
 
-})   
+})
+
 
 
 
